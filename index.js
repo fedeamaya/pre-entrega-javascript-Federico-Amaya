f7655936752dac0bce6125e68bf94f5830c201b0
federico amaya <fedeamaya88@hotmail.com>
2022-11-28 17:28:33 -0300

    commit dos

---
+++ index.js
@@ -0,0 +1,33 @@
+let nombreDelUsuario = prompt ("Ingresar nombre de usuario");
+
+if (nombreDelUsuario == "") {
+
+    alert("No ingresaste el nombre de usuario");
+}
+else {
+        alert("Nombre de usuario ingresado " + nombreDelUsuario);
+}
+
+let notaDelUsuario = prompt ("Ingrese su comentario");
+
+if (notaDelUsuario.length > 30) {
+
+    alert("Esta entrada excede la cantidad de caracteres disponibles.");
+}
+
+else if (notaDelUsuario == "") {
+    alert("No ingresaste ningun comentario");
+}
+
+else {
+    alert("Gracias por su comentario " + notaDelUsuario);
+}
+
+function saludar () {
+    alert (nombreDelUsuario + " " + notaDelUsuario);
+}
+saludar()
+
+for (let i = 0; i < 3; i++) {
+    confirm("El usuario ${i} te envio solicitud de amistad")
+}
