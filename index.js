4ddf972664c8e728f8f139ccf44005501986fe46
federico amaya <fedeamaya88@hotmail.com>
2022-12-29 18:06:35 -0300

    commit cinco

--- index.js
+++ index.js
@@ -10,31 +10,56 @@ botonModos.onclick = () => {
     }
 }
 
+const formLogin = document.querySelector("#login")
+const inputUser = document.querySelector("#username")
+const inputPass = document.querySelector("#password")
+const loginIncorrecto = document.querySelector("#login-incorrecto")
+const contenedorForm = document.querySelector(".container-login")
+const logout = document.querySelector("#login-out")
 
-const username = document.querySelector("username")
-const password = document.querySelector("password")
-const button = document.querySelector("button")
-const formlogin = document.querySelector("button")
-
-button.addEventListener('click', (event) => {
-    event.preventDefault()
-})
+const datosUsuario = {
+    user: "federico",
+    password: "coder123"
+}
 
 const subirAlLs = ( clave, valor ) => {
     localStorage.setItem(clave, JSON.stringify(valor))
 }
+
 const obtenerDelLs = ( clave ) => {
     return JSON.parse(localStorage.getItem(clave))
 }
 
-const datosUsuario = {
-    username: "federico",
-    password: "coder123"
-}
-
-formlogin.onsubmit = ( event ) => {
+formLogin.onsubmit = ( event ) => {
     event.preventDefault()
-    if ( username.value === datosUsuario.user && password.value === datosUsuario.password ) {
-        subirAlLs("login", true)             
+    if ( inputUser.value === datosUsuario.user && inputPass.value === datosUsuario.password ) {
+        subirAlLs("login", true)
+        contenedorForm.style.display = "none"  
+        logout.style.display = "block"              
+    } else {        
+        loginIncorrecto.style.display = "block"
+        inputPass.style.border = "1px solid red"
+        inputUser.style.border = "1px solid red"
     }
 }
+
+function validarLogin ( clave ) {
+    if ( clave !== true ) {
+        contenedorForm.style.display = "flex"
+        logout.style.display = "none"        
+    } else {
+        contenedorForm.style.display = "none"
+        logout.style.display = "block"        
+    }
+}
+
+validarLogin(obtenerDelLs("login"))
+
+logout.onclick = () => {
+    localStorage.removeItem( "login" )
+    console.log("cerrar sesion")
+    validarLogin(obtenerDelLs("login"))
+    formLogin.reset()
+}
+
+
